21e97fc546a190b7ac221506ac8da44ccfb47c83
AenuHub <jesuskane92@gmail.com>
2024-10-28 15:18:21 +0300

    add list implementation

--- W5_Lists2/Program.cs
+++ W5_Lists2/Program.cs
@@ -1,3 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<string> coffeeNames = new List<string>();
+        Console.WriteLine("Please enter 5 coffee names in order");
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write($"Coffee {i + 1}: ");
+            string coffeeName = Console.ReadLine();
+            coffeeNames.Add(coffeeName);
+        }
 
-Console.WriteLine("Hello, World!");
+        Console.WriteLine("--------------------------------");
+        
+        // Display the list of coffee names
+        Console.WriteLine("The list of coffee names:");
+        foreach (string coffeeName in coffeeNames)
+        {
+            Console.WriteLine(coffeeName);
+        }
+        
+        Console.ReadKey();
+    }
+}
